40df22b16ed43119438d50a2c5c59b611f3a5027
Dwi Aji <si.aji03@gmail.com>
2022-12-12 08:36:42 +0700

    WIP: Blog Model and Migration

---
+++ app/Models/Blog/Category.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models\Blog;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public $table = 'blog_categories';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        //
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        // 
+    ];
+    
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
+
+    /**
+     * Primary Key Relation
+     * 
+     * @return model
+     */
+
+    /**
+     * Foreign Key Relation
+     * 
+     * @return model
+     */
+
+    /**
+     * The "boot" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Listen to Create Event
+        static::creating(function ($model) {
+            // Always generate UUID on Data Create
+            $model->{'uuid'} = (string) Str::uuid();
+        });
+    }
+}
---
+++ database/migrations/2022_12_09_065324_create_blog_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->index();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('author_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+};
